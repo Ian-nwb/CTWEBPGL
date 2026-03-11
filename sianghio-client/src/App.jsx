0fc6e32b98e39491ada7c0a2b711bb9e3178f6ce
Ian <kennethsianghio756@gmail.com>
2026-03-11 12:40:31 +0800

    initial commit

---
+++ sianghio-client/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react'; import './App.css';
+function App() {
+return (
+<div className="App">
+<header className="App-header"> <h1>Welcome to My React App!</h1>
+<p>
+Name: Ian Kenneth R. Sianghio<br />
+Email: kennethsianghio756@gmail.com<br   /> 
+Other Personal Info: Lebron James Glazer
+</p>
+</header>
+</div>
+);
+}
+export default App;
